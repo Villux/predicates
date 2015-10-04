aa2f91d3293eeb8e64fe5b7f24fde0af9b8d4e1e
Ville Toiviainen <villej.toiviainen@gmail.com>
2015-10-04 14:26:25 +0300

    partial implementation of exercise

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,37 +1,84 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
+
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
+
+(def graphic-novels [{:name "Yotsuba 1" :series "Yotsuba"}
+                     {:name "Yotsuba 5" :series "Yotsuba"}
+                     {:name "Persepolis"}
+                     {:name "That Yellow Bastard" :series "Sin City"}
+                     {:name "The Hard Goodbye" :series "Sin City"}
+                     {:name "Maus"}
+                     {:name "Solanin"}
+                     {:name "Monster 3" :series "Monster"}])
+
+
+(filter #{1 2 3} [0 1 2 3])
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [a-key] (contains? a-set a-key)))
+
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
+
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
+(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
+
+(def authors #{china, octavia, kay, dick, zelazny})
+
+(def cities {:title "The City and the City" :authors #{china}
+             :awards #{:locus, :world-fantasy, :hugo}})
+(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
+                    :awards #{:hugo}})
+(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
+(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
+(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
+
+(def books #{cities, wild-seed, lord-of-light,
+             deus-irae, ysabel, scanner-darkly})
+
+(:title scanner-darkly)
+
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
+
+;; This was my first solution for this, but it has unneeded steps
+;; (every? true? (map #(has-award? book %))
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [award] (has-award? book award)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [ filtered-list (filter identity (map pred a-seq))]
+    (if (empty? filtered-list)
+      false
+      (first filtered-list))))
+
 
 (defn my-every? [pred a-seq]
   :-)
