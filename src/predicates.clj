d58b8126c025d0e28cfabad071e8e1c2f9e9f780
Ville Toiviainen <villej.toiviainen@gmail.com>
2015-10-04 14:43:30 +0300

    test passed

--- src/predicates.clj
+++ src/predicates.clj
@@ -10,17 +10,6 @@
 (defn equal-to [n]
   (fn [k] (== k n)))
 
-(def graphic-novels [{:name "Yotsuba 1" :series "Yotsuba"}
-                     {:name "Yotsuba 5" :series "Yotsuba"}
-                     {:name "Persepolis"}
-                     {:name "That Yellow Bastard" :series "Sin City"}
-                     {:name "The Hard Goodbye" :series "Sin City"}
-                     {:name "Maus"}
-                     {:name "Solanin"}
-                     {:name "Monster 3" :series "Monster"}])
-
-
-(filter #{1 2 3} [0 1 2 3])
 
 (defn set->predicate [a-set]
   (fn [a-key] (contains? a-set a-key)))
@@ -38,30 +27,6 @@
 
 (defn blank? [string]
   (every? whitespace? string))
-
-(def china {:name "China Miéville", :birth-year 1972})
-(def octavia {:name "Octavia E. Butler"
-              :birth-year 1947
-              :death-year 2006})
-(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
-(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
-(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
-
-(def authors #{china, octavia, kay, dick, zelazny})
-
-(def cities {:title "The City and the City" :authors #{china}
-             :awards #{:locus, :world-fantasy, :hugo}})
-(def wild-seed {:title "Wild Seed", :authors #{octavia}})
-(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
-                    :awards #{:hugo}})
-(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
-(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
-(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
-
-(def books #{cities, wild-seed, lord-of-light,
-             deus-irae, ysabel, scanner-darkly})
-
-(:title scanner-darkly)
 
 
 (defn has-award? [book award]
@@ -81,8 +46,9 @@
 
 
 (defn my-every? [pred a-seq]
-  :-)
+  (every? true? (map pred a-seq)))
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [pred (fn [int] (= 0 (mod n int)))]
+    (not (some pred (range 2 n)))))
+
